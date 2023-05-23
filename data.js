65273304a32bcc9db181843751191d80caa60f57
AleGCarranza <109099016+AleGCarranza@users.noreply.github.com>
2023-05-22 19:15:30 -0600

    adding news

---
+++ data.js
@@ -0,0 +1,23 @@
+export default  {
+    articles: [
+        {
+        "author": "ESPN",
+        "title": "Juventus fue sancionado y le quitaron 10 puntos",
+        "description": "Otro de los cambios que se vislumbran en América es la salida de Jürgen Damm.El mediocampista de 30 años regresó a la Liga MX en el Apertura 2022 tras un paso por la MLS, sin embargo no sumó más de 90 minutos de juego en ningún torneo.De acuerdo también a información de Ibáñez el jugador emigrará al Atlético de San Luis.",
+        "url": "https://news.google.com/rss/articles/CBMicWh0dHBzOi8vd3d3LmVzcG4uY29tLm14L2Z1dGJvbC9pdGFsaWEvbm90YS9fL2lkLzEyMDg4NTgxL2p1dmVudHVzLXNhbmNpb24tcXVpdGEtMTAtcHVudG9zLXNlcmllLWEtY2Fzby1wbHVzdmFsaWFz0gF-aHR0cHM6Ly93d3cuZXNwbi5jb20ubXgvZnV0Ym9sL2l0YWxpYS9ub3RhL18vaWQvMTIwODg1ODEvanV2ZW50dXMtc2FuY2lvbi1xdWl0YS0xMC1wdW50b3Mtc2VyaWUtYS1jYXNvLXBsdXN2YWxpYXM_cGxhdGZvcm09YW1w?oc=5",
+        "urlToImage": "https://i.ibb.co/BnZt1sV/imagen-deportes.png",
+        "publishedAt": "2023-05-22T19:41:15Z",
+        "content": null
+        },
+        {
+    
+        "author": "ESPN",
+        "title": "América: Roger Martínez encabeza bajas tras eliminación vs Chivas",
+        "description": "Las Águilas tienen como prioridad reforzar la lateral derecha. De acuerdo a información de Julio Ibáñez, reportero de TUDN, esta posición es la más urgente para el conjunto azulcrema pues buscan que Emilio Lara compita como defensa central. La lista de candidatos para la lateral derecha la integran Kevin Álvarez (Pachuca), Brian 'Guamerucito' García (Toluca), Ricardo Chávez (Atlético de San Luis) y Diego Barbosa (Atlas).",
+        "url": "https://news.google.com/rss/articles/CBMieWh0dHBzOi8vd3d3LmVzcG4uY29tLm14L2Z1dGJvbC9tZXhpY28vbm90YS9fL2lkLzEyMDkwNTg3L2FtZXJpY2Etcm9nZXItbWFydGluZXotZW5jYWJlemEtYmFqYXMtdHJhcy1lbGltaW5hY2lvbi12cy1jaGl2YXPSAYYBaHR0cHM6Ly93d3cuZXNwbi5jb20ubXgvZnV0Ym9sL21leGljby9ub3RhL18vaWQvMTIwOTA1ODcvYW1lcmljYS1yb2dlci1tYXJ0aW5lei1lbmNhYmV6YS1iYWphcy10cmFzLWVsaW1pbmFjaW9uLXZzLWNoaXZhcz9wbGF0Zm9ybT1hbXA?oc=5",
+        "urlToImage": "https://i.ibb.co/Hx678nt/Screenshot-4.png",
+        "publishedAt": "2023-05-22T19:29:42Z",
+        "content": null
+        },
+        
+       ]    }
